86c901fcfe10efc119033e977ada400be69c594d
Paulo Rodriguez <paulo_crr@hotmail.com>
2021-11-29 22:18:35 -0500

    refactor: cors enabled for all the origins

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-let corsOptions = {
-    origin: "http://localhost:8080"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
